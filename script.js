caaaf3b93da271b21bdd7a551f7fc5eea51ffceb
Garibov2001 <mahmood.garibov@gmail.com>
2020-11-09 11:54:35 +0400

    Borders and score is done.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 
-console.log(window.innerWidth)
 
-
-var left_value = 0; //Default value
-var top_value = 0; //Default value
+var left_value = 30; //Default value
+var top_value = 30; //Default value
 var interval_id = 0 //Default value
 var interval_time = 100 //0.1 second
-
+var check_interval_id = 0
+var score = 0
 
 document.addEventListener("keydown", event => {
     if (event.keyCode === 37) 
@@ -27,6 +26,47 @@ document.addEventListener("keydown", event => {
     }
   });
 
+
+// Check current position
+function CheckCurrentPosition()
+{
+    if (top_value < 30 || top_value > 540 || left_value < 30 || left_value > 1290 )
+    {  
+        RestartGame()
+    }
+    else if(IsBoxOnTarget())
+    {
+        score += 1
+        $('#score').html(score)
+    }
+} 
+
+
+function IsBoxOnTarget()
+{
+    if (top_value == parseInt($('#target-box').css("top").split('px')[0]) &&
+    left_value == parseInt($('#target-box').css("left").split('px')[0]))
+    {
+        return true
+    } 
+    return false
+}
+
+function RestartGame()
+{
+    clearInterval(interval_id);
+    left_value = 30; //Default value
+    top_value = 30; //Default value
+    score = 0
+    $('#score').html(score)
+    $('#snake-box').css("left", `${left_value}px`);  
+    $('#snake-box').css("top", `${top_value}px`);  
+    alert("You losed the game..")
+}
+
+
+
+
 function LeftDirection()
 {
     clearInterval(interval_id);
@@ -34,7 +74,9 @@ function LeftDirection()
         left_value = parseInt($('#snake-box').css("left").split('px')[0])
         left_value -= 30 
         $('#snake-box').css("left", `${left_value}px`);
+        CheckCurrentPosition()
     }, interval_time); // 0.2 sec
+    
 }
 
 function TopDirection()
@@ -44,6 +86,7 @@ function TopDirection()
         top_value = parseInt($('#snake-box').css("top").split('px')[0])
         top_value -= 30 
         $('#snake-box').css("top", `${top_value}px`); 
+        CheckCurrentPosition()
     }, interval_time); // 0.2 sec
 }
 
@@ -54,6 +97,7 @@ function RightDirection()
         left_value = parseInt($('#snake-box').css("left").split('px')[0])
         left_value += 30 
         $('#snake-box').css("left", `${left_value}px`); 
+        CheckCurrentPosition()
     }, interval_time); // 0.2 sec
 
 }
@@ -64,6 +108,8 @@ function DownDirection()
     interval_id = setInterval(function(){
         top_value = parseInt($('#snake-box').css("top").split('px')[0])
         top_value += 30 
-        $('#snake-box').css("top", `${top_value}px`); 
+        $('#snake-box').css("top", `${top_value}px`);         
+        CheckCurrentPosition()
     }, interval_time); // 0.2 sec
 }
+
